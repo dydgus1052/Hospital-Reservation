b9dd6d4eda023752837cec15bdce472a109f38a7
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-03-13 00:40:10 +0900

    20230313 add Address Info

--- src/main/java/hospital/hospitalReservation/domain/Address.java
+++ src/main/java/hospital/hospitalReservation/domain/Address.java
@@ -8,14 +8,16 @@ import lombok.Getter;
 public class Address {
 
     private String city;
+    private String street;
     private String zipcode;
 
     protected Address() {
 
     }
 
-    public Address(String city, String zipcode) {
+    public Address(String city, String street, String zipcode) {
         this.city = city;
+        this.street = street;
         this.zipcode = zipcode;
     }
 }
